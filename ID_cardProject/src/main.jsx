232b052c2a25f4e7b2056376ad5f710b055f1246
NikhilHarshe <nikhilharshe123@gmail.com>
2024-06-26 11:51:23 +0530

    second commit

--- ID_cardProject/src/main.jsx
+++ ID_cardProject/src/main.jsx
@@ -3,16 +3,20 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Provider } from "react-redux";
-import { BrowserRouter } from 'react-router-dom'
+// import { BrowserRouter } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
 import { store } from './redux/Store.jsx';
+import { HashRouter } from 'react-router-dom';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <BrowserRouter>
+    {/* <BrowserRouter> */}
+    <HashRouter>
       <App />
       <Toaster />
-    </BrowserRouter>
+    </HashRouter>
+
+    {/* </BrowserRouter> */}
   </Provider>
 )
